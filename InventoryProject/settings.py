b77059a696e6d591e27c788327246e19ed18af05
AbnerCestari <abner_cestari@hotmail.com>
2023-02-21 16:43:46 -0300

    added layer of security

--- InventoryProject/settings.py
+++ InventoryProject/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from dotenv import load_dotenv
+load_dotenv()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-STATTICFILES_DIRS = [
+STATICFILES_DIRS = [
     BASE_DIR/ "static"
 ]
 
