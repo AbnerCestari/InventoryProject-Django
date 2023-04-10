eedd484b9acd171bbe50069c6df2bf61c5e5c031
AbnerCestari <abner_cestari@hotmail.com>
2023-04-10 19:31:49 -0300

    Minor updates

--- InventoryProject/settings.py
+++ InventoryProject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from dotenv import load_dotenv
 load_dotenv()
 from pathlib import Path
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!gyb(+nx7sju_#y@ls06vi7+ixda)#^@^3#)syde(uk-_m1itx'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
